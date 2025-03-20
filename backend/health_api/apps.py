b7f569bf248cfc3e3e82fdd6ec64b2dcdbabee3d
ISrbz <159147342+ISrbz@users.noreply.github.com>
2025-03-20 13:20:58 +0200

    renamed django project

--- backend/health_api/apps.py
+++ backend/health_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PainsConfig(AppConfig):
+class HealthApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pains'
+    name = 'health_api'
