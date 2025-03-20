d92e7a99e66546a6c0188205dee1de501e5df262
ISrbz <159147342+ISrbz@users.noreply.github.com>
2025-03-20 15:17:50 +0200

    created empty database and test html page

--- backend/health_api/models.py
+++ backend/health_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Injury(models.Model):
+    place = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    symptoms = models.TextField()
+    recommendations = models.TextField()
+    
--- backend/health_api/views.py
+++ backend/health_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Injury
+
+def injuries(request):
+    injuriess = Injury.objects.all().values()
+    template = loader.get_template('basic_display_all.html')
+    context = {'injuries': injuriess,}
+    return HttpResponse(template.render(context, request))
+'''
+
+from django.http import HttpResponse
+from django.template import loader
+
+def hello(request):
+  template = loader.get_template('hello.html')
+  return HttpResponse(template.render())'''
