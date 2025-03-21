43c207a34984759c5ae14676a0d566a6e46b0244
ISrbz <159147342+ISrbz@users.noreply.github.com>
2025-03-21 06:54:18 +0200

    made database a bit more complex

--- backend/health_api/models.py
+++ backend/health_api/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
 class Injury(models.Model):
     place = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     symptoms = models.TextField()
-    recommendations = models.TextField()
-    
+    recommendations = models.TextField(default="Консултирайте се с медицинско лице.")
+
+
+class Symptom(models.Model):
+    place = models.IntegerField(null=True)
+    name = models.CharField(max_length=255)
+    key = models.CharField(max_length=10)
+
+    def save(self, *args, **kwargs):
+        self.key = str(self.place) + self.name[:3]
+        super(Symptom, self).save(*args, **kwargs)
+
+class Cause(models.Model):
+    name = models.CharField(max_length=255)
+    symptoms = models.ForeignKey(Symptom, on_delete=models.CASCADE, null=True)
+    #symp = Symptom.key
--- backend/health_api/views.py
+++ backend/health_api/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Injury
+from .models import Symptom, Cause
 
 def injuries(request):
-    injuriess = Injury.objects.all().values()
+    symptomss = Symptom.objects.all().values()
+    causess = Cause.objects.all().values()
     template = loader.get_template('basic_display_all.html')
-    context = {'injuries': injuriess,}
+    context = {
+        'causess': causess,
+        'symptomss': symptomss,
+        }
     return HttpResponse(template.render(context, request))
